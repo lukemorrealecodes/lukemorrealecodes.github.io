535fee520dc3011e2219f12f209fc1e573108c29
lukemorrealecodes <lukemorreale14@gmail.com>
2025-02-02 23:14:23 -0700

    Add files via upload

---
+++ vecchioWebsite01/Project01/js/java.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function javaMessage() {
+    return prompt("Hello! First, let's start with your name!");
+}
+
+var userName = javaMessage();
+
+
+alert("Welcome, " + userName + "! Thank you for visiting!");
